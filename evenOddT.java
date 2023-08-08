c954ff913f59f7a9ef14f600e820040699c56306
CHAUHANKUSHAGRA9459 <114767400+CHAUHANKUSHAGRA9459@users.noreply.github.com>
2023-08-08 23:06:19 +0530

    Add files via upload

---
+++ evenOddT.java
@@ -0,0 +1,12 @@
+public class evenOddT {
+    public static void main(String[] Args) {
+
+        int a = 10;
+        int con = a%2==0?1:0;
+        if(con==1)
+            System.out.println("EVEN");
+        else
+            System.out.println("ODD");
+
+    }
+}
